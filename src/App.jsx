d4934defdbc08c9872725b7f863e826ca9fa68f9
ananyabagla <129501826+ananyabagla@users.noreply.github.com>
2025-06-20 12:32:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordGenerator from "./components/PasswordGenerator";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center p-4">
+      <PasswordGenerator />
+    </div>
+  );
+};
+
+export default App;
